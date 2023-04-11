9e0781400665e5720b3545ed709d549ed4967eb3
hwatso02 <hwatso02@gmail.com>
2023-04-10 21:59:28 -0600

    Better passwords and make empty CRUD Pages

--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,17 @@ namespace Intex2
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Default Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 12;
+                options.Password.RequiredUniqueChars = 1;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
